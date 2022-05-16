814a3b676db750877e5ab73ae126e14372825750
pauldbarham <103535616+pauldbarham@users.noreply.github.com>
2022-05-16 15:53:14 -0500

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "instruqt-circleci"
+    organization = "barhamco"
     workspaces {
       name = "hashicat-aws"
     }
